3bc3610069d8cf2167d4b592c41135db2963cc87
SunSung W541 <mail@embedded.biz.ua>
2024-06-12 17:44:53 +0200

    main [v1.1.1]  actions

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package TimeRules
 const (
 	GlobalName       string = "TimeRules"
 	GlobalDateUpdate string = "06-12-2024"
-	GlobalHash       string = "c3a9117360b2d9f2102180bdffafac045c9d95b2"
+	GlobalHash       string = "96fd8e899596bb61d89d749dfddece9bbbb62f94"
 
-	GlobalVersion      string = "v1.1.0"
+	GlobalVersion      string = "v1.1.1"
 	GlobalVersionMajor string = "v1"
 	GlobalVersionMinor uint16 = 1
-	GlobalVersionPatch uint16 = 0
+	GlobalVersionPatch uint16 = 1
 )
