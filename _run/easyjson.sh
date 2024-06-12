e5d0c59f94b716e47606fc5a18b54ac7b2ad6877
SunSung W541 <mail@embedded.biz.ua>
2024-06-12 17:15:02 +0200

    main [v1.0.3]  easyjson

--- _run/easyjson.sh
+++ _run/easyjson.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
+root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+root_dir="$(dirname "$root_dir")"
+
+#############################################################################
 
 echo "Обход структуры Time-Rules"
-easyjson -all -omit_empty struct.go
-easyjson -all -omit_empty def.go
+easyjson -all -omit_empty "$root_dir/struct.go"
+easyjson -all -omit_empty "$root_dir/def.go"
 
